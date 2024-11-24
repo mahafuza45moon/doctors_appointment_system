36e432d9e422ca7f3cd14a73d2d25d05279aef61
Samia907 <20201038@uap-bd.edu>
2024-11-24 11:18:49 +0600

    add bootstarap

--- doctors_appointment_system/settings.py
+++ doctors_appointment_system/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'doctors_appointment_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- member/forms.py
+++ member/forms.py
@@ -3,12 +3,32 @@ from django.contrib.auth.models import User
 from .models import AppUser
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'})
+    )
     user_type = forms.ChoiceField(choices=AppUser.USER_TYPE_CHOICES, initial='patient', widget=forms.HiddenInput())
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}),
+        }
+        help_texts = {
+            'username': None,
+        }
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your username'
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your password'
+        })
+    )
